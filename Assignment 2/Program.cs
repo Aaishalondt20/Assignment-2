db55de39e429e225774485f60513bc80f6f53bba
aaish <aaish@AAISHA>
2023-12-12 19:24:27 +0200

    Add project files.

---
+++ Assignment 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+
+
+        {
+            Console.WriteLine("Enter Up To 4 Digits");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(NumbersToWords.ConvertNumberToWords(input));
+        }
+
+    }
+
+}
